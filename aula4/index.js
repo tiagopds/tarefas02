24dff2674f46659c69357ce1d02b0b7ee64db27a
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-10 12:09:27 -0300

    conversão de variaveis

--- aula4/index.js
+++ aula4/index.js
@@ -1,1 +1,13 @@
-console.log("hello world!")
+// olá mundo
+console.log("Hello World!")
+//Primeira execução de variáveis
+const nome = "Tiago"
+ const idade = 34
+ 
+ console.log("Meu nome é:",nome,"e tenho", idade,"anos de idade" )
+ // teste
+ const idadeNumero = 23
+ const idadeTexto = idadeNumero.toString()
+ console.log(typeof idadeNumero)
+ console.log(typeof idadeTexto)
+
