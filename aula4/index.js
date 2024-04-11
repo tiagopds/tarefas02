2aac25ad1930e9ab2bb6767b65e617a279e0c50a
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-11 11:43:07 -0300

    Exercicios aula 11_04

--- aula4/index.js
+++ aula4/index.js
@@ -4,10 +4,11 @@ console.log("Hello World!")
 const nome = "Tiago"
  const idade = 34
  
- console.log("Meu nome é:",nome,"e tenho", idade,"anos de idade" )
+ console.log("Meu nome é:",nome,"e tenho", idade,"anos de idade",)
  // teste
  const idadeNumero = 23
  const idadeTexto = idadeNumero.toString()
+ 
  console.log(typeof idadeNumero)
  console.log(typeof idadeTexto)
 
