289dde0d161df9adebf0753e53b7d6dff7ea7a24
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-29 12:00:09 -0300

    funções_aula_29_04_2024

---
+++ aulaFunções_29_04_2024/index.js
@@ -0,0 +1,39 @@
+
+// AULA DE FUNÇÕES DIA: 29/04/2024
+
+/*function imprimirOlaMundo(){
+alert("olá Mundo!")
+
+}
+imprimirOlaMundo()
+imprimirOlaMundo()
+imprimirOlaMundo()
+*/
+
+/*function imprimirNome(nome){
+alert(`Olá ${nome}`)
+}
+let NomeUser = ""
+NomeUser = prompt("Digite seu nome:")
+imprimirNome(NomeUser)
+NomeUser = prompt("Digite seu nome:")
+imprimirNome(NomeUser)
+NomeUser = prompt("Digite seu nome:")
+imprimirNome(NomeUser)
+
+*/
+
+
+//EXERCICÍOS :03 FUNÇÕES
+// RECEBA DOIS NUMEROS E  RETORNE A SOMA ENTRE ELES:
+
+
+function somaNum(num1,num2){
+    let soma = num1 + num2
+    return soma
+}
+let numero1 = 2
+let numero2 = 12
+let resultado = 0
+resultado = somaNum(numero1,numero2)
+alert(resultado)
