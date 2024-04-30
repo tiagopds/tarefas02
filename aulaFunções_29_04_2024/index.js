c0ff9ef6e5d1d6c69926dcf9ff3674a6d678928b
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-30 09:39:42 -0300

    aula_29_04_2024_ex04

--- aulaFunções_29_04_2024/index.js
+++ aulaFunções_29_04_2024/index.js
@@ -29,7 +29,7 @@ imprimirNome(NomeUser)
 //EXERCICÍOS: 03 FUNÇÕES
 //RECEBA DOIS NUMEROS E  RETORNE A SOMA ENTRE ELES:
 
-function somaNum(num1,num2){
+/*function somaNum(num1,num2){
     let soma = num1 + num2
     return soma
 }
@@ -38,3 +38,25 @@ let numero2 = 12
 let resultado = 0
 resultado = somaNum(numero1,numero2)
 alert(resultado)
+*/
+//------------------------------------------------------------------------------------------
+
+//Exercicío 04:
+function trataArray(newArray){
+  // inicializa variáveis
+let array = []
+let tamanho = newArray.length
+
+// Armazena primeiro valor dividido por 2
+array.push((newArray[0]/2))
+array.push((newArray[(tamanho-1)]/2))
+// Retorna array nova
+return array
+}
+// Declara variáveis
+let array = [2,3,4,5,6,7,8]
+let novaArray = []
+// Chama função
+novaArray = trataArray(array)
+// Escreve com alert
+alert(novaArray)
